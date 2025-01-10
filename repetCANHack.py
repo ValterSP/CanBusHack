76f99678f9ded3557a6e20516fb73799a829a7b6
Valter Pedrosa <2210991@my.ipleiria.pt>
2025-01-10 22:20:12 +0000

    add comments

--- repetCANHack.py
+++ repetCANHack.py
@@ -1,36 +1,38 @@
 import can
 import time
 
-# Set up the virtual CAN interface
+# Cria o bus onde serão enviados os comandos
 bus = can.interface.Bus(channel='vcan0', bustype='socketcan')
 
-# Create the messages to send
+#Codigo enviado com o id 0x188 com os bytes 0x01, 0x00, 0x00 que faz o pisca para a esquerda
 turnLeftMsg = can.Message(arbitration_id=0x188, data=[0x01, 0x00, 0x00], is_extended_id=False)
 
+#Dados enviados com o id 0x188 com os bytes 0x02, 0x00, 0x00 que faz o pisca para a direita
 turnRightMsg = can.Message(arbitration_id=0x188, data=[0x02, 0x00, 0x00], is_extended_id=False)
-
+#Dados enviados com o id 0x188 com os bytes 0x00, 0x00, 0x00 para desligar os piscas
 noTurnMsg = can.Message(arbitration_id=0x188, data=[0x00, 0x00, 0x00], is_extended_id=False)
-
+#Dados enviados com o id 0x244 onde os ultimos 2 bytes representam a velocidade no velocimetro
 accMsg = can.Message(arbitration_id=0x244, data=[0x00, 0x00, 0x00, 0x38, 0x94], is_extended_id=False)
-
+#Dados enviados com o id 0x198 para abrir a porta direita de trás e as outras fechadas
 openDoor4Msg =  can.Message(arbitration_id=0x19B,data=[0x00, 0x00, 0x07],is_extended_id=False)
-
+#Dados enviados com o id 0x198 para abrir a porta esquerda de trás e as outras fechadas
 openDoor3Msg =  can.Message(arbitration_id=0x19B, data=[0x00, 0x00, 0x0B], is_extended_id=False)
-
+#Dados enviados com o id 0x198 para abrir a porta direita da frente e as outras fechadas
 openDoor2Msg =  can.Message(arbitration_id=0x19B, data=[0x00, 0x00, 0x0D], is_extended_id=False)
-
+#Dados enviados com o id 0x198 para abrir a porta esquerda da frente e as outras fechadas
 openDoor1Msg =  can.Message(arbitration_id=0x19B, data=[0x00, 0x00, 0x0E], is_extended_id=False)
-
+#Dados enviados com o id 0x198 para trancar todas as portas
 closeAllDoorsMsg = can.Message( arbitration_id=0x19B, data=[0x00, 0x00, 0x0F], is_extended_id=False)
-
+#Dados enviados com o id 0x198 para abrir todas as portas
 openAllDoorsMsg = can.Message(arbitration_id=0x19B, data=[0x00, 0x00, 0x00], is_extended_id=False)
-
+#Dados enviados com o id 0x198 para abrir as duas portas da frente
 open12DoorMsg = can.Message(arbitration_id=0x19B, data=[0x00, 0x00, 0x0C], is_extended_id=False)
-
+#Dados enviados com o id 0x198 para abrir as duas portas da frente e a esquerda de trás
 open123DoorMsg = can.Message(arbitration_id=0x19B, data=[0x00, 0x00, 0x08], is_extended_id=False)
-
+#Dados enviados com o id 0x244 para colocar a velocidade do velocimetro a 0
 stopMsg = can.Message(arbitration_id=0x244, data=[0x00, 0x00, 0x00, 0x00, 0x00], is_extended_id=False)
 
+#enviar mensagem no CAN bus para ligar o pisca
 def turn_message_bus(msg):
     num_msg = 0
     while num_msg < 10:
@@ -40,6 +42,7 @@ def turn_message_bus(msg):
             time.sleep(0.1)
             num_msg += 1
 
+#enviar mensagem no CAN bus para abrir e fechar as portas
 def open_close_door_bus(openDoor):
     num_msg = 0
     while num_msg < 10:
@@ -49,11 +52,7 @@ def open_close_door_bus(openDoor):
             num_msg += 1
             time.sleep(0.1)
 
-
-def increment_data(msg, increment_value=1):
-    msg.data = [(byte + increment_value) % 256 for byte in msg.data]
-    return msg
-
+#enviar mensagem no CAN bus para aumentar velocidade
 def accelerate_bus(accMsg):
     num_msg = 0
     while num_msg < 10:
@@ -63,6 +62,7 @@ def accelerate_bus(accMsg):
         time.sleep(0.2)
         num_msg += 1
 
+#enviar várias mensagens para o CAN bus com várias instruções
 def crazy_mode_bus():
     num_msg = 0
     while num_msg < 10:
